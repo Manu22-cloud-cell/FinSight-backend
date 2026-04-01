726672621890a1eaa29bc1eae67ce0880f2d46d6
Manu22-cloud-cell <mailtomanojky@gmail.com>
2026-04-01 13:28:33 +0530

    Added JWT auth middleware for protected routes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
+const { protect } = require("../middlewares/authMiddleware");
 
 router.post("/register", authController.register);
 router.post("/login", authController.login);
+
+router.get("/me", protect, (req, res) => {
+    res.json({
+        message: "Protected route accessed",
+        user: req.user
+    });
+});
 
 module.exports = router;
