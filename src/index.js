8a2c1496696133ca783f5b5a277c98b022f9acc9
Manu22-cloud-cell <mailtomanojky@gmail.com>
2026-04-26 11:53:23 +0530

    corrected the sequence of db connection + scheduler + server start to avoid restarting of server

--- src/index.js
+++ src/index.js
@@ -1,41 +1,50 @@
 require("dotenv").config();
+
 const app = require("./app");
 const http = require("http");
 const { Server } = require("socket.io");
 const connectDB = require("./config/db");
+const setupRepeatableJobs = require("./queues/scheduler");
 
 const PORT = process.env.PORT || 3000;
 
-connectDB();
+const startServer = async () => {
+  try {
+    // Connect to DB
+    await connectDB();
+    console.log("MongoDB connected");
 
-const setupRepeatableJobs = require("./queues/scheduler");
+    //Setup jobs
+    await setupRepeatableJobs();
 
-setupRepeatableJobs();
+    const server = http.createServer(app);
 
-const server = http.createServer(app);
+    const io = new Server(server, {
+      cors: {
+        origin: "*",
+      },
+    });
 
-const io = new Server(server, {
-  cors: {
-    origin: "*",
-  },
-});
+    global.io = io;
 
-global.io = io;
+    io.on("connection", (socket) => {
+      console.log("User connected:", socket.id);
 
-io.on("connection", (socket) => {
-  console.log("User connected:", socket.id);
+      socket.on("join", (userId) => {
+        socket.join(userId);
+        console.log("User joined room:", userId);
+      });
+    });
 
-  socket.on("join", (userId) => {
-    socket.join(userId);
-    console.log("User joined room:", userId);
-  });
-});
+    // Start server
+    server.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
 
-server.listen(PORT, () => {
-  console.log("Server running on port 3000");
-});
+  } catch (error) {
+    console.error("Failed to start server:", error);
+    process.exit(1); //prevents infinite PM2 restart loop
+  }
+};
 
-
-
-
-
+startServer();
